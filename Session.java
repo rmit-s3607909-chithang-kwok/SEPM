9094b388ef1f5e85d890c6daf442b11955b1ab5f
rmit-s3607909-chithang-kwok <31905455+rmit-s3607909-chithang-kwok@users.noreply.github.com>
2018-05-01 12:08:14 +1000

    Add files via upload

--- Session.java
+++ Session.java
@@ -2,17 +2,33 @@ import java.util.*;
 import java.util.ArrayList;
 
 public class Session{
-Booking[] Booking =  new Booking[20];
+ArrayList[] Booking =  new ArrayList[20];
 private int date;
 private int id;
 
-	public Session() {
-		// TODO Auto-generated
-		
+	public Session(int date, int id) {
+		this.date = date;
+		this.id=id;
 	     
 	}
 
-	public String MakeBooking() {
+	public int getid() {
+		return id;
+	}
+	
+	public int getdate() {
+		return date;
+	}
+	
+	public void setid() {
+		this.id = id;
+	}
+	
+	public void setdate() {
+		this.date=date;
+	}
+	
+	public static void MakeBooking() {
 		int input;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Please select the Movie you want to watch?");
@@ -22,20 +38,35 @@ private int id;
 		
 		input = sc.nextInt();
 		System.out.println("Your option  are" + input);
+		
+		
+		
+		
 		System.out.println("Thank you for your booking.");
 		
 		
 		}
 	
 		
-		return 
-	}
+	
+	
 
-	public String DeleteBooking() {
-		return
+	public static void DeleteBooking() {
+		int input;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please select the Movie you want to delete?");
+		System.out.println("1. Avenger3");
+		System.out.println("2. Black Panther");
+		System.out.println("3. Rampage");
+		
+		input = sc.nextInt();
+		System.out.println("Your option  are" + input);
+		System.out.println("Movie has been deleted.");
+		
 	}
+		
+}	
 	
-	
-}
+
 
 
