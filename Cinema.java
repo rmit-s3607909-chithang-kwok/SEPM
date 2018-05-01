300fd2bb00013c608acffc3f6e0d1e5ca1f6ce16
rmit-s3439333-nisal-wijethilake <31201736+rmit-s3439333-nisal-wijethilake@users.noreply.github.com>
2018-05-01 12:24:42 +1000

    nisal

--- Cinema.java
+++ Cinema.java
@@ -7,6 +7,14 @@ public class Cinema{
 		String id;
 		
 		ArrayList movieSessions = new ArrayList();
+		
+		public void listSessions()
+		{
+			for(int i=0; i<movieSessions.size(); i++) {
+				System.out.println(movieSessions.get(i). +":"+ movieSessions.get(i).mark +", "+ movieSessions.get(i).grade);
+			}
+		}
+		
 	}
 
 }
