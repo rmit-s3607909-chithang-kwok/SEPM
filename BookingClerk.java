29b7dcab9b64d1098be0757b21fe3714e66b4cd2
rmit-s3607909-chithang-kwok <31905455+rmit-s3607909-chithang-kwok@users.noreply.github.com>
2018-05-14 15:53:08 +1000

    Add files via upload

--- BookingClerk.java
+++ BookingClerk.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
 
 public class BookingClerk {
 	private String username;
 	private String password;
 	private String id;
+	 private static ArrayList<BookingClerk> bookingClerks = new ArrayList<BookingClerk>();
 	
 	public BookingClerk(String username, String password, String id) {
 		this.username=username;
@@ -10,4 +12,22 @@ public class BookingClerk {
 		this.id=id;
 	}
 
+	public String getUsername(){
+		return username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public String getid() {
+		return id;
+	}
+
+	public void addBookingClerk(BookingClerk BookingClerk)
+	{
+		bookingClerks.add(BookingClerk);
+	}
 }
+
+
